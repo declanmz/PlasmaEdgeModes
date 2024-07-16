0b030a22e53b95bfebdd4602838b9ec1c88e7e40
Declan Murphy Zink <declanmurphyzink@gmail.com>
2024-07-16 14:50:48 -0700

    Finished intersection code for Eavg, Estd, Emax inclusion and plotting on dispersion relation

--- WaveguideIntersectionToJSON.py
+++ WaveguideIntersectionToJSON.py
@@ -45,9 +45,11 @@ if thetadegs != thetadegsJSON:
 wp0 = jsondata['wp0']
 k0list = np.asarray(jsondata['k0list'])
 evals_list = list(map(np.array, jsondata['evals_list']))
+Eavg_list = list(map(np.array, jsondata['Eavg_list']))
+Estd_list = list(map(np.array, jsondata['Estd_list']))
+Emax_list = list(map(np.array, jsondata['Emax_list']))
 
-
-#Waveguide dispersion
+# Waveguide dispersion
 dwg = 6.25e-3
 # wpwg = 2 * np.pi * 3.5e9
 wpwg = wp0
@@ -56,54 +58,90 @@ dispersionload = np.loadtxt("C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/Waveg
 k0waveguide = dispersionload[:,0]/180 /dwg * np.pi / kpwg
 w_waveguide = dispersionload[:,1] * 2 * np.pi * 1e9
 
-#Interpolate to k0list **negative side**
-
+# Interpolate to k0list **negative side**
+# kinda stupid to be splitting these up and have duplicate code, but it's useful later
+# Overall this is just bad code, but whatever it was the fastest to write
 wgEpsilon = 1e9
 
 neg_k0Ids = np.where(k0list < 0)[0].tolist()
 neg_k0list = k0list[neg_k0Ids]
 neg_evals_list = [evals_list[i] for i in neg_k0Ids]
+neg_Eavg_list = [Eavg_list[i] for i in neg_k0Ids]
+neg_Estd_list = [Estd_list[i] for i in neg_k0Ids]
+neg_Emax_list = [Emax_list[i] for i in neg_k0Ids]
+
 neg_wgInterpolation = np.interp(neg_k0list, (-k0waveguide)[::-1], w_waveguide[::-1])
 
 neg_wgDistance = np.array([])
 neg_wgEval = np.array([])
+neg_wgEavg = np.array([])
+neg_wgEstd = np.array([])
+neg_wgEmax = np.array([])
+
 for i in range(len(neg_k0list)):
     closestEvalId = np.argmin(np.abs(neg_evals_list[i].real-neg_wgInterpolation[i]))
     neg_wgDistance = np.append(neg_wgDistance, np.abs(neg_evals_list[i][closestEvalId].real - neg_wgInterpolation[i]))
     neg_wgEval = np.append(neg_wgEval, neg_evals_list[i][closestEvalId].real)
+    neg_wgEavg = np.append(neg_wgEavg, neg_Eavg_list[i][closestEvalId].real)
+    neg_wgEstd = np.append(neg_wgEstd, neg_Estd_list[i][closestEvalId].real)
+    neg_wgEmax = np.append(neg_wgEmax, neg_Emax_list[i][closestEvalId].real)
 
 neg_epsilonIndices = np.where(neg_wgDistance < wgEpsilon)
 neg_epsilon_k0list = neg_k0list[neg_epsilonIndices]
 neg_epsilon_wgDistance = neg_wgDistance[neg_epsilonIndices]
 neg_epsilon_wgEval = neg_wgEval[neg_epsilonIndices]
+neg_epsilon_wgEavg = neg_wgEavg[neg_epsilonIndices]
+neg_epsilon_wgEstd = neg_wgEstd[neg_epsilonIndices]
+neg_epsilon_wgEmax = neg_wgEmax[neg_epsilonIndices]
 
-#Interpolate to k0list **positive side**
+# Interpolate to k0list **positive side**
 pos_k0Ids = np.where(k0list > 0)[0].tolist()
 pos_k0list = k0list[pos_k0Ids]
 pos_evals_list = [evals_list[i] for i in pos_k0Ids]
+pos_Eavg_list = [Eavg_list[i] for i in pos_k0Ids]
+pos_Estd_list = [Estd_list[i] for i in pos_k0Ids]
+pos_Emax_list = [Emax_list[i] for i in pos_k0Ids]
+
 pos_wgInterpolation = np.interp(pos_k0list, k0waveguide, w_waveguide)
 
 pos_wgDistance = np.array([])
 pos_wgEval = np.array([])
+pos_wgEavg = np.array([])
+pos_wgEstd = np.array([])
+pos_wgEmax = np.array([])
+
 for i in range(len(pos_k0list)):
     closestEvalId = np.argmin(np.abs(pos_evals_list[i].real-pos_wgInterpolation[i]))
     pos_wgDistance = np.append(pos_wgDistance, np.abs(pos_evals_list[i][closestEvalId].real - pos_wgInterpolation[i]))
     pos_wgEval = np.append(pos_wgEval, pos_evals_list[i][closestEvalId].real)
+    pos_wgEavg = np.append(pos_wgEavg, pos_Eavg_list[i][closestEvalId].real)
+    pos_wgEstd = np.append(pos_wgEstd, pos_Estd_list[i][closestEvalId].real)
+    pos_wgEmax = np.append(pos_wgEmax, pos_Emax_list[i][closestEvalId].real)
 
 pos_epsilonIndices = np.where(pos_wgDistance < wgEpsilon)
 pos_epsilon_k0list = pos_k0list[pos_epsilonIndices]
 pos_epsilon_wgDistance = pos_wgDistance[pos_epsilonIndices]
 pos_epsilon_wgEval = pos_wgEval[pos_epsilonIndices]
+pos_epsilon_wgEavg = pos_wgEavg[pos_epsilonIndices]
+pos_epsilon_wgEstd = pos_wgEstd[pos_epsilonIndices]
+pos_epsilon_wgEmax = pos_wgEmax[pos_epsilonIndices]
 
 # ---------- Update JSON File ----------
 jsondata['wgEpsilon'] = wgEpsilon
+
 jsondata['wgNeg_k0list'] = neg_epsilon_k0list
 jsondata['wgNeg_distanceList'] = neg_epsilon_wgDistance
 jsondata['wgNeg_evalList'] = neg_epsilon_wgEval
-#add Eavg, Estd, Emax to these lists --> then can do colormap plots or alpha plots
+jsondata['wgNeg_EavgList'] = neg_epsilon_wgEavg
+jsondata['wgNeg_EstdList'] = neg_epsilon_wgEstd
+jsondata['wgNeg_EmaxList'] = neg_epsilon_wgEmax
+
 jsondata['wgPos_k0list'] = pos_epsilon_k0list
 jsondata['wgPos_distanceList'] = pos_epsilon_wgDistance
 jsondata['wgPos_evalList'] = pos_epsilon_wgEval
+jsondata['wgPos_EavgList'] = pos_epsilon_wgEavg
+jsondata['wgPos_EstdList'] = pos_epsilon_wgEstd
+jsondata['wgPos_EmaxList'] = pos_epsilon_wgEmax
 
 
 with open(file, 'w') as f: 
