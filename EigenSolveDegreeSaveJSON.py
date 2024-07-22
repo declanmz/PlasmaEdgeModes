updated to work with correct normalization. Still need to plot e-vectors

EigenSolveDegreeSaveJSON.py
@@ -8,85 +8,70 @@ import json
 
 from JSONHelpers import TypeEncoder, as_complex
 
-# ---------- Define Constants ----------
-e = 1.6e-19 #C - charge on electron
-m_e = 9.1094e-31 #kg - mass of electron
-al = m_e/e #define constant used in matrix
-alin = 1/al #al inverse
-mu_0 = 4e-7*np.pi #H/m - permeability of free space
-ep_0 = 8.854e-12 #F/m - permitivity of free space
-c = 3e8 #m/s - speed of light
-
-
 # ---------- Computation Functions ----------
-def BuildMatrix(ky, kz, N, B0, wplist, eplist, mulist, deltax):
-    #L is length of x bounds [-L, L], in meters
-    #N is the total number of points
-    #ky, kz wavenumbers
-    #B0 strength of magnetic field in T (in z-direction)
-    #wp is plasma frequency as function of x
-    #ep is background permitivity as function of x
-    #mu is background permeability as function of x
+def BuildMatrix(ky, kz, N, wc, wplist, eplist, deltax):
+    #All inputs in terms of dimensionless variables (assume _n)
 
     M = np.matrix(np.zeros((9*N, 9*N), dtype=np.complex128))
     
     for im in range(0, N):
         i = 9*im
-        #vxi row
-        M[i, i+3] = -1j*alin
-        M[i, i+1] = -1j*alin*B0
+        #uxi row
+        M[i, i+1] = -1j*wc #uyi
+        M[i, i+3] = -1j #Exi
         
-        #vyi row
-        M[i+1, i+4] = -1j*alin
-        M[i+1, i] = 1j*alin*B0
+        #uyi row
+        M[i+1, i] = 1j*wc #uxi
+        M[i+1, i+4] = -1j #Eyi
         
-        #vzi row
-        M[i+2, i+5] = -1j*alin
+        #uzi row
+        M[i+2, i+5] = -1j #Ezi
     
         #Exi row
-        M[i+3, (i-1)%(9*N)] = -(c**2 * ky)/(2*mulist[im]*eplist[im])
-        M[i+3, i+8] = -(c**2 * ky)/(2*mulist[im]*eplist[im])
-        M[i+3, (i-2)%(9*N)] = (c**2 * kz)/(2*mulist[im]*eplist[im])
-        M[i+3, i+7] = (c**2 * kz)/(2*mulist[im]*eplist[im])
-        M[i+3, i] = (1j*al*wplist[im]**2)/(eplist[im])
+        M[i+3, i] = (1j*wplist[im]**2)/(eplist[im]) #uxi
+        M[i+3, (i-2)%(9*N)] = (kz)/(2*eplist[im]) #By(i-1/2)
+        M[i+3, i+7] = (kz)/(2*eplist[im]) #By(i+1/2)
+        M[i+3, (i-1)%(9*N)] = -(ky)/(2*eplist[im]) #Bz(i-1/2)
+        M[i+3, i+8] = -(ky)/(2*eplist[im]) #Bz(i+1/2)
         
         #Eyi row
-        M[i+4, (i-3)%(9*N)] = -(c**2 * kz)/(2*mulist[im]*eplist[im])
-        M[i+4, i+6] = -(c**2 * kz)/(2*mulist[im]*eplist[im])
-        M[i+4, i+8] = -(1j * c**2)/(deltax * mulist[im]*eplist[im])
-        M[i+4, (i-1)%(9*N)] = (1j * c**2)/(deltax * mulist[im]*eplist[im])
-        M[i+4, i+1] = (1j*al*wplist[im]**2)/(eplist[im])
+        M[i+4, i+1] = (1j*wplist[im]**2)/(eplist[im]) #uyi
+        M[i+4, (i-3)%(9*N)] = -(kz)/(2*eplist[im]) #Bx(i-1/2)
+        M[i+4, i+6] = -(kz)/(2*eplist[im]) #Bx(i+1/2)
+        M[i+4, i+8] = -(1j)/(deltax*eplist[im]) #Bz(i+1/2)
+        M[i+4, (i-1)%(9*N)] = (1j)/(deltax*eplist[im]) #Bz(i-1/2)
         
         #Ezi row
-        M[i+5, i+7] = (1j * c**2)/(deltax * mulist[im]*eplist[im])
-        M[i+5, (i-2)%(9*N)] = -(1j * c**2)/(deltax * mulist[im]*eplist[im])
-        M[i+5, (i-3)%(9*N)] = (c**2 * ky)/(2*mulist[im]*eplist[im])
-        M[i+5, i+6] = (c**2 * ky)/(2*mulist[im]*eplist[im])
-        M[i+5, i+2] = (1j*al*wplist[im]**2)/(eplist[im])
+        M[i+5, i+2] = (1j*wplist[im]**2)/(eplist[im]) #uzi
+        M[i+5, (i-3)%(9*N)] = (ky)/(2*eplist[im]) #Bx(i-1/2)
+        M[i+5, i+6] = (ky)/(2*eplist[im]) #Bx(i+1/2)
+        M[i+5, i+7] = (1j)/(deltax*eplist[im]) #By(i+1/2)
+        M[i+5, (i-2)%(9*N)] = -(1j)/(deltax*eplist[im]) #By(i-1/2)
         
         #Bx(i+1/2) row
-        M[i+6, i+5] = ky/2
-        M[i+6, (i+14)%(9*N)] = ky/2
-        M[i+6, i+4] = -kz/2
-        M[i+6, (i+13)%(9*N)] = -kz/2
+        M[i+6, i+5] = ky/2 #Ezi
+        M[i+6, (i+14)%(9*N)] = ky/2 #Ez(i+1)
+        M[i+6, i+4] = -kz/2 #Eyi
+        M[i+6, (i+13)%(9*N)] = -kz/2 #Ey(i+1)
         
         #By(i+1/2) row
-        M[i+7, i+3] = kz/2
-        M[i+7, (i+12)%(9*N)] = kz/2
-        M[i+7, (i+14)%(9*N)] = 1j/deltax
-        M[i+7, i+5] = -1j/deltax
+        M[i+7, i+3] = kz/2 #Exi
+        M[i+7, (i+12)%(9*N)] = kz/2 #Ex(i+1)
+        M[i+7, (i+14)%(9*N)] = 1j/deltax #Ez(i+1)
+        M[i+7, i+5] = -1j/deltax #Ezi
         
         #Bz(i+1/2) row
-        M[i+8, (i+13)%(9*N)] = -1j/deltax
-        M[i+8, i+4] = 1j/deltax
-        M[i+8, i+3] = -ky/2
-        M[i+8, (i+12)%(9*N)] = -ky/2
+        M[i+8, (i+13)%(9*N)] = -1j/deltax #Ey(i+1)
+        M[i+8, i+4] = 1j/deltax #Eyi
+        M[i+8, i+3] = -ky/2 #Exi
+        M[i+8, (i+12)%(9*N)] = -ky/2 #Ex(i+1)
 
-        #RIGHT NOW THIS HAS PERIODIC BOUNDARY CONDITIONS
+        #THIS HAS PERIODIC BOUNDARY CONDITIONS
 
     return M
 
 def EAvgStdMaxList(evecMatrix, xlist, N):
+    #All inputs in terms of dimensionless variables (assume _n)
     listLen = evecMatrix.shape[1]
     Eavg = [None] * listLen
     Estd = [None] * listLen
@@ -104,13 +89,13 @@ def EAvgStdMaxList(evecMatrix, xlist, N):
     
     return np.array(Eavg), np.array(Estd), np.array(Emax)
 
-def CutSortedEigensolve(k0mag, kzoffset, wmin, wmax, directory, theta, N, B0, wplist, eplist, mulist, deltax, xlist, wp0):
-    pbarInner = tqdm(total=4, desc=f"Eigensolve {k0mag}", position=1, leave=False)
-    kmag = wp0 * k0mag / c
+def CutSortedEigensolve(kmag, kzoffset, wmin, wmax, theta, N, wc, wplist, eplist, deltax, xlist):
+    #All inputs in terms of dimensionless variables (assume _n)
+    pbarInner = tqdm(total=4, desc=f"Eigensolve {kmag}", position=1, leave=False)
     ky = kmag * np.sin(theta)
     kz = kmag * np.cos(theta) + kzoffset
 
-    eigsys = np.linalg.eig(BuildMatrix(ky, kz, N, B0, wplist, eplist, mulist, deltax))
+    eigsys = np.linalg.eig(BuildMatrix(ky, kz, N, wc, wplist, eplist, deltax))
     pbarInner.update(1)
 
     #sort in order of increasing eigenvalue
@@ -141,120 +126,146 @@ def CutSortedEigensolve(k0mag, kzoffset, wmin, wmax, directory, theta, N, B0, wp
     return cutsort_evals, cutsort_Eavg, cutsort_Estd, cutsort_Emax
 
 # ---------- main function ----------
-def EigenSolveDegree_SaveJSON(directory, fp0, B0, L, N, w0min, w0max, wp, ep, mu, k0min, k0max, thetadegs, Nk, kzoffset):
+def EigenSolveDegree_SaveJSON(directory, fr, B0, L, N, fmin, fmax, wp, ep, kmin, kmax, thetadegs, Nk, kzoffset, fp0):
+
+    #All normalized variables writen as x_n, all dimensioned variables written as normal
+
     # ---------- Define Constants ----------
     e = 1.6e-19 #C - charge on electron
     m_e = 9.1094e-31 #kg - mass of electron
-    al = m_e/e #define constant used in matrix
-    alin = 1/al #al inverse
     mu_0 = 4e-7*np.pi #H/m - permeability of free space
     ep_0 = 8.854e-12 #F/m - permitivity of free space
     c = 3e8 #m/s - speed of light
 
     # ---------- Setup Parameters ----------
-    wp0 = 2*np.pi*fp0
-    wc = B0/al
+    wr = 2*np.pi*fr
+    wc = e*B0/m_e
     fc = wc / (2 *np.pi)
-    wmin = w0min * wp0
-    wmax = w0max * wp0
+    wc_n = wc/wr
+    wmin = 2*np.pi*fmin
+    wmax = 2*np.pi*fmax
+    wmin_n = wmin/wr
+    wmax_n = wmax/wr
 
+    kmin_n = c*kmin/wr
+    kmax_n = c*kmax/wr
+    kzoffset_n = c*kzoffset/wr
 
+    L_n = wr*L/c
     deltax = 2*L / N #Division betweeen points
-    xlist = [(-L + (i+0.5)*deltax) for i in range(0,N)] #List of x points
+    deltax_n = wr*deltax/c
+    xlist = np.asarray([(-L + (i+0.5)*deltax) for i in range(0,N)]) #List of x points
+    xlist_n = wr*xlist/c
     
-    wplist = [wp(xi) for xi in xlist]
-    eplist = [ep(xi) for xi in xlist]
-    mulist = [mu(xi) for xi in xlist]
+    wplist = np.asarray([wp(xi) for xi in xlist])
+    wplist_n = wplist/wr
+    eplist = np.asarray([ep(xi) for xi in xlist])
 
-    k0list = np.linspace(k0min,k0max,Nk)
+    klist_n = np.linspace(kmin_n, kmax_n, Nk)
     theta = thetadegs / 180 * np.pi
 
     # ---------- Run For Specific Degree ----------
-    evals_list = []
-    Eavg_list = []
-    Estd_list = []
-    Emax_list = []
+    evals_list_n = []
+    Eavg_list_n = []
+    Estd_list_n = []
+    Emax_list_n = []
 
     pbar = tqdm(total=Nk, desc=f"{thetadegs}deg Computation", position=0)
-    for k0 in k0list:
-        evals, Eavg, Estd, Emax = CutSortedEigensolve(k0, kzoffset, wmin, wmax, directory, 
-                                                      theta, N, B0, wplist, eplist, mulist, deltax, xlist, wp0)
+    for k_n in klist_n:
+        evals_n, Eavg_n, Estd_n, Emax_n = CutSortedEigensolve(k_n, kzoffset_n, wmin_n, wmax_n, theta, N, wc_n, wplist_n, eplist, deltax_n, xlist_n)
 
-        evals_list.append(evals)
-        Eavg_list.append(Eavg)
-        Estd_list.append(Estd)
-        Emax_list.append(Emax)
+        evals_list_n.append(evals_n)
+        Eavg_list_n.append(Eavg_n)
+        Estd_list_n.append(Estd_n)
+        Emax_list_n.append(Emax_n)
 
         pbar.update(1)
 
     save_dict = {
         'fp0':fp0,
-        'wp0':wp0,
+        'wp0':2*np.pi*fp0,
+        'fr':fr,
+        'wr':wr,
         'B0':B0,
         'N':N,
         'L':L,
+        'L_n':L_n,
         'wc':wc,
+        'wc_n':wc_n,
         'fc':fc,
         'wmin':wmin,
+        'wmin_n':wmin_n,
         'wmax':wmax,
+        'wmax_n':wmax_n,
         'deltax':deltax,
+        'deltax_n':deltax_n,
         'xlist':xlist,
+        'xlist_n':xlist_n,
         'wplist':wplist,
+        'wplist_n':wplist_n,
         'eplist':eplist,
-        'mulist':mulist,
-        'k0min':k0min,
-        'k0max':k0max,
+        'kmin':kmin,
+        'kmin_n':kmin_n,
+        'kmax':kmax,
+        'kmax_n':kmax_n,
         'thetadegs':thetadegs,
         'theta':theta,
         'Nk':Nk,
         'kzoffset':kzoffset,
-        'k0list':k0list,
-        'evals_list':evals_list,
-        'Eavg_list':Eavg_list,
-        'Estd_list':Estd_list,
-        'Emax_list':Emax_list
+        'kzoffset_n':kzoffset_n,
+        'klist_n':klist_n,
+        'evals_list_n':evals_list_n,
+        'Eavg_list_n':Eavg_list_n,
+        'Estd_list_n':Estd_list_n,
+        'Emax_list_n':Emax_list_n
     }
 
-    dict_file = directory + f'/{thetadegs}deg_CutSort.json'
+    dict_file = directory + f'/{thetadegs}deg_Unfiltered.json'
     with open(dict_file, 'w') as f: 
         json.dump(save_dict, f, cls=TypeEncoder)
 
-# # ---------- Setup parameters ----------
-# fp0 = 10e9 #10GHz
-# B0 = 87e-3 #mT
-# L = 15e-3 * 2 #7.5 mm
-# N = 300
-# w0min = 0.05
-# w0max = 1.5
+# ---------- Setup parameters ----------
+scaling = 1
 
-# wp0 = 2*np.pi*fp0
+fp0 = 10e9 #10GHz
+B0 = 87e-3 #mT
+L = 15e-3 * scaling
+N = 100 #300 for good results
+fr = 1e9 #1 GHz
+fmin = 0.5 * fr
+fmax = 15 * fr
 
-# # ---------- Plasma Density Profile ----------
-# Lscale = 4e-3 #1mm
-# qstart = 6.5e-3 *2 #mm
-# offset = qstart - 2.5e-3
-# qthickness = 1e-3 #1mm
+wp0 = 2*np.pi*fp0
 
-# def wp(x): #plasma frequency as a function of x
+# ---------- Plasma Density Profile ----------
+Lscale = 2e-3 * scaling#1mm
+qstart = 6.5e-3 * scaling #mm
+offset = qstart - 1.25e-3 * scaling
+qthickness = 1e-3 #1mm
+
+def wp(x): #plasma frequency as a function of x
+    if np.abs(x) >= qstart:
+        return 0
+    return wp0*0.5*(np.tanh((x+offset)/Lscale) - np.tanh((x-offset)/Lscale))
+
+# def wp(x): #quadratic density profile
 #     if np.abs(x) >= qstart:
 #         return 0
-#     return wp0*0.5*(np.tanh((x+offset)/Lscale) - np.tanh((x-offset)/Lscale))
+#     return (-wp0/(qstart**2))*(x-qstart)*(x+qstart)
 
-# def ep(x): #relative permitivity of background medium (not including plasma)
-#     if np.abs(x) > qstart and np.abs(x) < qstart + qthickness:
-#         return 4
-#     return 1
-    
-# def mu(x): #relative permeability of background medium (not including plasma)
-#     return 1
+def ep(x): #relative permitivity of background medium (not including plasma)
+    if np.abs(x) > qstart and np.abs(x) < qstart + qthickness:
+        return 4
+    return 1
 
-# # ---------- ky kz Line at Specific Angle ----------
-# k0min = -4
-# k0max = 4
-# Nk = 100
-# kzoffset = 0
+# ---------- ky kz Line at Specific Angle ----------
+kmin = -500 #1/m
+kmax = 500 #1/m
+Nk = 20 #100 for good results
+kzoffset = 0
 
-# directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/Testing'
-# thetadegs = 38
+directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/Testing'
+thetadegs = 39
 
 # EigenSolveDegree_SaveJSON(directory, fp0, B0, L, N, w0min, w0max, wp, ep, mu, k0min, k0max, thetadegs, Nk, kzoffset)
+EigenSolveDegree_SaveJSON(directory, fr, B0, L, N, fmin, fmax, wp, ep, kmin, kmax, thetadegs, Nk, kzoffset, fp0)

V1/EigenJsonFiltering.py
@@ -22,29 +22,26 @@ def filterEigJSON(file, filterName, directory, filter_posAvgBound, filter_negAvg
     with open(file, 'r') as f:
         jsondata = json.load(f, object_hook=as_complex)
 
-    wr = jsondata['wr']
     thetadegsJSON = jsondata['thetadegs']
     if thetadegs != thetadegsJSON:
         raise Exception("Thetadegs mismatch")
 
-    evals_list_n = list(map(np.array, jsondata['evals_list_n']))
-    Eavg_list_n = list(map(np.array, jsondata['Eavg_list_n']))
-    Eavg_list = [c*val/wr for val in Eavg_list_n]
-    Estd_list_n = list(map(np.array, jsondata['Estd_list_n']))
-    Estd_list = [c*val/wr for val in Estd_list_n]
-    Emax_list_n = list(map(np.array, jsondata['Emax_list_n']))
+    evals_list = list(map(np.array, jsondata['evals_list']))
+    Eavg_list = list(map(np.array, jsondata['Eavg_list']))
+    Estd_list = list(map(np.array, jsondata['Estd_list']))
+    Emax_list = list(map(np.array, jsondata['Emax_list']))
 
     # ---------- Filtering ----------
     for i in range(jsondata['Nk']):
         EavgFilteredIndices = np.where((Eavg_list[i].real < filter_posAvgBound) & (Eavg_list[i].real > filter_negAvgBound))
         EstdFilteredIndices = np.where((Estd_list[i].real < filter_maxStd))
-        EmaxFilteredIndices = np.where((Emax_list_n[i].real > filter_EmaxMin))
+        EmaxFilteredIndices = np.where((Emax_list[i].real > filter_EmaxMin))
         filteredIndices = np.intersect1d(np.intersect1d(EavgFilteredIndices, EstdFilteredIndices), EmaxFilteredIndices)
 
-        jsondata['evals_list_n'][i] = evals_list_n[i][filteredIndices]
-        jsondata['Eavg_list_n'][i] = Eavg_list_n[i][filteredIndices]
-        jsondata['Estd_list_n'][i] = Estd_list_n[i][filteredIndices]
-        jsondata['Emax_list_n'][i] = Emax_list_n[i][filteredIndices]
+        jsondata['evals_list'][i] = evals_list[i][filteredIndices]
+        jsondata['Eavg_list'][i] = Eavg_list[i][filteredIndices]
+        jsondata['Estd_list'][i] = Estd_list[i][filteredIndices]
+        jsondata['Emax_list'][i] = Emax_list[i][filteredIndices]
 
 
     # ---------- Save As New JSON ----------
@@ -60,14 +57,14 @@ def filterEigJSON(file, filterName, directory, filter_posAvgBound, filter_negAvg
     print('Filtering Done.')
 
 
-directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/Testing'
-thetadegs = 39
-file = directory + f'/{thetadegs}deg_Unfiltered.json'
+# directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/SetupOG'
+# thetadegs = 38
+# file = directory + f'/{thetadegs}deg_CutSort.json'
 
-filterName = 'FilterA'
-filter_posAvgBound = 15e-3
-filter_negAvgBound = -2.5e-3
-filter_maxStd = 1 #4e-3
-filter_EmaxMin = 1e-5
+# filterName = 'FilterB'
+# filter_posAvgBound = 15e-3
+# filter_negAvgBound = 0
+# filter_maxStd = 1 #4e-3
+# filter_EmaxMin = 1e-5
 
-filterEigJSON(file, filterName, directory, filter_posAvgBound, filter_negAvgBound, filter_maxStd, filter_EmaxMin, thetadegs)
+# filterEigJSON(file, filterName, directory, filter_posAvgBound, filter_negAvgBound, filter_maxStd, filter_EmaxMin, thetadegs)

V1/RunAngleSweep_NoQuartzA.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from EigenSolveDegreeSaveJSON import EigenSolveDegree_SaveJSON
-from EigenJsonFiltering import filterEigJSON
+from PlasmaEdgeModes.V1.EigenSolveDegreeSaveJSON import EigenSolveDegree_SaveJSON
+from PlasmaEdgeModes.V1.EigenJsonFiltering import filterEigJSON
 from WaveguideIntersectionToJSON import findWgIntersection_UpdateJSON
 
 # ---------- Define Constants ----------
@@ -14,29 +14,29 @@ ep_0 = 8.854e-12 #F/m - permitivity of free space
 c = 3e8 #m/s - speed of light
 
 # ---------- Setup parameters ----------
-fp0 = 10e9 #10GHz
+# NoQuartzA has no quartz, a smooth plasma profile,
+# lower plasma frequency of 6 GHz, and the actually correct length
+
+fp0 = 6e9 #10GHz
 B0 = 87e-3 #mT
-L = 15e-3 * 2 #7.5 mm
-N = 300
+L = 15e-3
+N = 300 #300
 w0min = 0.05
 w0max = 1.5
 
 wp0 = 2*np.pi*fp0
 
 # ---------- Plasma Density Profile ----------
-Lscale = 4e-3 #1mm
-qstart = 6.5e-3 *2 #mm
-offset = qstart - 2.5e-3
-qthickness = 1e-3 #1mm
+Lscale = 2e-3 #mm
+# qstart = 6.5e-3 *2 #mm
+# offset = qstart - 2.5e-3
+# qthickness = 1e-3 #1mm
+offset = 7.5e-3
 
 def wp(x): #plasma frequency as a function of x
-    if np.abs(x) >= qstart:
-        return 0
     return wp0*0.5*(np.tanh((x+offset)/Lscale) - np.tanh((x-offset)/Lscale))
 
 def ep(x): #relative permitivity of background medium (not including plasma)
-    if np.abs(x) > qstart and np.abs(x) < qstart + qthickness:
-        return 4
     return 1
     
 def mu(x): #relative permeability of background medium (not including plasma)
@@ -45,16 +45,16 @@ def mu(x): #relative permeability of background medium (not including plasma)
 # ---------- ky kz Line at Specific Angle ----------
 k0min = -2.5
 k0max = 2.5
-Nk = 100
+Nk = 100 #100
 kzoffset = 0
 
 
-directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/SetupOG_ReducedRange'
-thetadegsList = np.arange(0,40,2)
+directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/Setup_NoQuartzA'
+thetadegsList = np.arange(0,40,2) #2
 
 filterName = 'FilterA'
 filter_posAvgBound = 15e-3
-filter_negAvgBound = -5e-3
+filter_negAvgBound = -2.5e-3
 filter_maxStd = 1 #4e-3
 filter_EmaxMin = 1e-5
 

WaveguideIntersectionToJSON.py
@@ -8,12 +8,10 @@ import json
 
 from JSONHelpers import TypeEncoder, as_complex
 
-def findWgIntersection_UpdateJSON(file, wgEpsilon, thetadegs):
+def findWgIntersection_UpdateJSON(file, wgEpsilon_n, thetadegs):
     # ---------- Define Constants ----------
     e = 1.6e-19 #C - charge on electron
     m_e = 9.1094e-31 #kg - mass of electron
-    al = m_e/e #define constant used in matrix
-    alin = 1/al #al inverse
     mu_0 = 4e-7*np.pi #H/m - permeability of free space
     ep_0 = 8.854e-12 #F/m - permitivity of free space
     c = 3e8 #m/s - speed of light
@@ -26,105 +24,103 @@ def findWgIntersection_UpdateJSON(file, wgEpsilon, thetadegs):
     if thetadegs != thetadegsJSON:
         raise Exception("Thetadegs mismatch")
 
-    wp0 = jsondata['wp0']
-    k0list = np.asarray(jsondata['k0list'])
-    evals_list = list(map(np.array, jsondata['evals_list']))
-    Eavg_list = list(map(np.array, jsondata['Eavg_list']))
-    Estd_list = list(map(np.array, jsondata['Estd_list']))
-    Emax_list = list(map(np.array, jsondata['Emax_list']))
+    wr = jsondata['wr']
+    klist_n = np.asarray(jsondata['klist_n'])
+    klist = wr*klist_n/c
+    evals_list_n = list(map(np.array, jsondata['evals_list_n']))
+    Eavg_list_n = list(map(np.array, jsondata['Eavg_list_n']))
+    Estd_list_n = list(map(np.array, jsondata['Estd_list_n']))
+    Emax_list_n = list(map(np.array, jsondata['Emax_list_n']))
 
     # Waveguide dispersion
     dwg = 6.25e-3
-    # wpwg = 2 * np.pi * 3.5e9
-    wpwg = wp0
-    kpwg = wpwg/c
     dispersionload = np.loadtxt("C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/WaveguideDispersion/eps_4.txt")
-    k0waveguide = dispersionload[:,0]/180 /dwg * np.pi / kpwg
-    w_waveguide = dispersionload[:,1] * 2 * np.pi * 1e9
+    k_waveguide = dispersionload[:,0]/180 /dwg * np.pi
+    w_waveguide_n = dispersionload[:,1] * 2 * np.pi * 1e9 / wr
 
-    # Interpolate to k0list **negative side**
+    # Interpolate to klist **negative side**
     # kinda stupid to be splitting these up and have duplicate code, but it's useful later
     # Overall this is just bad code, but whatever it was the fastest to write
 
-    neg_k0Ids = np.where(k0list < 0)[0].tolist()
-    neg_k0list = k0list[neg_k0Ids]
-    neg_evals_list = [evals_list[i] for i in neg_k0Ids]
-    neg_Eavg_list = [Eavg_list[i] for i in neg_k0Ids]
-    neg_Estd_list = [Estd_list[i] for i in neg_k0Ids]
-    neg_Emax_list = [Emax_list[i] for i in neg_k0Ids]
+    neg_k0Ids = np.where(klist < 0)[0].tolist()
+    neg_klist = klist[neg_k0Ids]
+    neg_evals_list_n = [evals_list_n[i] for i in neg_k0Ids]
+    neg_Eavg_list_n = [Eavg_list_n[i] for i in neg_k0Ids]
+    neg_Estd_list_n = [Estd_list_n[i] for i in neg_k0Ids]
+    neg_Emax_list_n = [Emax_list_n[i] for i in neg_k0Ids]
 
-    neg_wgInterpolation = np.interp(neg_k0list, (-k0waveguide)[::-1], w_waveguide[::-1])
+    neg_wgInterpolation = np.interp(neg_klist, (-k_waveguide)[::-1], w_waveguide_n[::-1])
 
     neg_wgDistance = np.array([])
-    neg_wgEval = np.array([])
-    neg_wgEavg = np.array([])
-    neg_wgEstd = np.array([])
-    neg_wgEmax = np.array([])
+    neg_wgEval_n = np.array([])
+    neg_wgEavg_n = np.array([])
+    neg_wgEstd_n = np.array([])
+    neg_wgEmax_n = np.array([])
 
-    for i in range(len(neg_k0list)):
-        closestEvalId = np.argmin(np.abs(neg_evals_list[i].real-neg_wgInterpolation[i]))
-        neg_wgDistance = np.append(neg_wgDistance, np.abs(neg_evals_list[i][closestEvalId].real - neg_wgInterpolation[i]))
-        neg_wgEval = np.append(neg_wgEval, neg_evals_list[i][closestEvalId].real)
-        neg_wgEavg = np.append(neg_wgEavg, neg_Eavg_list[i][closestEvalId].real)
-        neg_wgEstd = np.append(neg_wgEstd, neg_Estd_list[i][closestEvalId].real)
-        neg_wgEmax = np.append(neg_wgEmax, neg_Emax_list[i][closestEvalId].real)
+    for i in range(len(neg_klist)):
+        closestEvalId = np.argmin(np.abs(neg_evals_list_n[i].real-neg_wgInterpolation[i]))
+        neg_wgDistance = np.append(neg_wgDistance, np.abs(neg_evals_list_n[i][closestEvalId].real - neg_wgInterpolation[i]))
+        neg_wgEval_n = np.append(neg_wgEval_n, neg_evals_list_n[i][closestEvalId].real)
+        neg_wgEavg_n = np.append(neg_wgEavg_n, neg_Eavg_list_n[i][closestEvalId].real)
+        neg_wgEstd_n = np.append(neg_wgEstd_n, neg_Estd_list_n[i][closestEvalId].real)
+        neg_wgEmax_n = np.append(neg_wgEmax_n, neg_Emax_list_n[i][closestEvalId].real)
 
-    neg_epsilonIndices = np.where(neg_wgDistance < wgEpsilon)
-    neg_epsilon_k0list = neg_k0list[neg_epsilonIndices]
+    neg_epsilonIndices = np.where(neg_wgDistance < wgEpsilon_n)
+    neg_epsilon_klist = neg_klist[neg_epsilonIndices]
     neg_epsilon_wgDistance = neg_wgDistance[neg_epsilonIndices]
-    neg_epsilon_wgEval = neg_wgEval[neg_epsilonIndices]
-    neg_epsilon_wgEavg = neg_wgEavg[neg_epsilonIndices]
-    neg_epsilon_wgEstd = neg_wgEstd[neg_epsilonIndices]
-    neg_epsilon_wgEmax = neg_wgEmax[neg_epsilonIndices]
+    neg_epsilon_wgEval_n = neg_wgEval_n[neg_epsilonIndices]
+    neg_epsilon_wgEavg_n = neg_wgEavg_n[neg_epsilonIndices]
+    neg_epsilon_wgEstd_n = neg_wgEstd_n[neg_epsilonIndices]
+    neg_epsilon_wgEmax_n = neg_wgEmax_n[neg_epsilonIndices]
 
-    # Interpolate to k0list **positive side**
-    pos_k0Ids = np.where(k0list > 0)[0].tolist()
-    pos_k0list = k0list[pos_k0Ids]
-    pos_evals_list = [evals_list[i] for i in pos_k0Ids]
-    pos_Eavg_list = [Eavg_list[i] for i in pos_k0Ids]
-    pos_Estd_list = [Estd_list[i] for i in pos_k0Ids]
-    pos_Emax_list = [Emax_list[i] for i in pos_k0Ids]
+    # Interpolate to klist **positive side**
+    pos_k0Ids = np.where(klist > 0)[0].tolist()
+    pos_klist = klist[pos_k0Ids]
+    pos_evals_list_n = [evals_list_n[i] for i in pos_k0Ids]
+    pos_Eavg_list_n = [Eavg_list_n[i] for i in pos_k0Ids]
+    pos_Estd_list_n = [Estd_list_n[i] for i in pos_k0Ids]
+    pos_Emax_list_n = [Emax_list_n[i] for i in pos_k0Ids]
 
-    pos_wgInterpolation = np.interp(pos_k0list, k0waveguide, w_waveguide)
+    pos_wgInterpolation = np.interp(pos_klist, k_waveguide, w_waveguide_n)
 
     pos_wgDistance = np.array([])
-    pos_wgEval = np.array([])
-    pos_wgEavg = np.array([])
-    pos_wgEstd = np.array([])
-    pos_wgEmax = np.array([])
+    pos_wgEval_n = np.array([])
+    pos_wgEavg_n = np.array([])
+    pos_wgEstd_n = np.array([])
+    pos_wgEmax_n = np.array([])
 
-    for i in range(len(pos_k0list)):
-        closestEvalId = np.argmin(np.abs(pos_evals_list[i].real-pos_wgInterpolation[i]))
-        pos_wgDistance = np.append(pos_wgDistance, np.abs(pos_evals_list[i][closestEvalId].real - pos_wgInterpolation[i]))
-        pos_wgEval = np.append(pos_wgEval, pos_evals_list[i][closestEvalId].real)
-        pos_wgEavg = np.append(pos_wgEavg, pos_Eavg_list[i][closestEvalId].real)
-        pos_wgEstd = np.append(pos_wgEstd, pos_Estd_list[i][closestEvalId].real)
-        pos_wgEmax = np.append(pos_wgEmax, pos_Emax_list[i][closestEvalId].real)
+    for i in range(len(pos_klist)):
+        closestEvalId = np.argmin(np.abs(pos_evals_list_n[i].real-pos_wgInterpolation[i]))
+        pos_wgDistance = np.append(pos_wgDistance, np.abs(pos_evals_list_n[i][closestEvalId].real - pos_wgInterpolation[i]))
+        pos_wgEval_n = np.append(pos_wgEval_n, pos_evals_list_n[i][closestEvalId].real)
+        pos_wgEavg_n = np.append(pos_wgEavg_n, pos_Eavg_list_n[i][closestEvalId].real)
+        pos_wgEstd_n = np.append(pos_wgEstd_n, pos_Estd_list_n[i][closestEvalId].real)
+        pos_wgEmax_n = np.append(pos_wgEmax_n, pos_Emax_list_n[i][closestEvalId].real)
 
-    pos_epsilonIndices = np.where(pos_wgDistance < wgEpsilon)
-    pos_epsilon_k0list = pos_k0list[pos_epsilonIndices]
+    pos_epsilonIndices = np.where(pos_wgDistance < wgEpsilon_n)
+    pos_epsilon_klist = pos_klist[pos_epsilonIndices]
     pos_epsilon_wgDistance = pos_wgDistance[pos_epsilonIndices]
-    pos_epsilon_wgEval = pos_wgEval[pos_epsilonIndices]
-    pos_epsilon_wgEavg = pos_wgEavg[pos_epsilonIndices]
-    pos_epsilon_wgEstd = pos_wgEstd[pos_epsilonIndices]
-    pos_epsilon_wgEmax = pos_wgEmax[pos_epsilonIndices]
+    pos_epsilon_wgEval_n = pos_wgEval_n[pos_epsilonIndices]
+    pos_epsilon_wgEavg_n = pos_wgEavg_n[pos_epsilonIndices]
+    pos_epsilon_wgEstd_n = pos_wgEstd_n[pos_epsilonIndices]
+    pos_epsilon_wgEmax_n = pos_wgEmax_n[pos_epsilonIndices]
 
     # ---------- Update JSON File ----------
-    jsondata['wgEpsilon'] = wgEpsilon
+    jsondata['wgEpsilon_n'] = wgEpsilon_n
 
-    jsondata['wgNeg_k0list'] = neg_epsilon_k0list
+    jsondata['wgNeg_klist'] = neg_epsilon_klist
     jsondata['wgNeg_distanceList'] = neg_epsilon_wgDistance
-    jsondata['wgNeg_evalList'] = neg_epsilon_wgEval
-    jsondata['wgNeg_EavgList'] = neg_epsilon_wgEavg
-    jsondata['wgNeg_EstdList'] = neg_epsilon_wgEstd
-    jsondata['wgNeg_EmaxList'] = neg_epsilon_wgEmax
+    jsondata['wgNeg_evalList_n'] = neg_epsilon_wgEval_n
+    jsondata['wgNeg_EavgList_n'] = neg_epsilon_wgEavg_n
+    jsondata['wgNeg_EstdList_n'] = neg_epsilon_wgEstd_n
+    jsondata['wgNeg_EmaxList_n'] = neg_epsilon_wgEmax_n
 
-    jsondata['wgPos_k0list'] = pos_epsilon_k0list
+    jsondata['wgPos_klist'] = pos_epsilon_klist
     jsondata['wgPos_distanceList'] = pos_epsilon_wgDistance
-    jsondata['wgPos_evalList'] = pos_epsilon_wgEval
-    jsondata['wgPos_EavgList'] = pos_epsilon_wgEavg
-    jsondata['wgPos_EstdList'] = pos_epsilon_wgEstd
-    jsondata['wgPos_EmaxList'] = pos_epsilon_wgEmax
+    jsondata['wgPos_evalList_n'] = pos_epsilon_wgEval_n
+    jsondata['wgPos_EavgList_n'] = pos_epsilon_wgEavg_n
+    jsondata['wgPos_EstdList_n'] = pos_epsilon_wgEstd_n
+    jsondata['wgPos_EmaxList_n'] = pos_epsilon_wgEmax_n
 
 
     with open(file, 'w') as f: 
@@ -133,11 +129,11 @@ def findWgIntersection_UpdateJSON(file, wgEpsilon, thetadegs):
     print('Waveguide Intersection Done.')
 
 
-# #execute code for particular file
-# directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/SetupOG'
-# thetadegs = 38
-# filterName = 'FilterB'
-# file = directory + f'/{thetadegs}deg_{filterName}.json'
+#execute code for particular file
+directory = 'C:/Users/decla/Documents/SPPL/PlasmaEdgeModes/Testing'
+thetadegs = 39
+filterName = 'FilterA'
+file = directory + f'/{thetadegs}deg_{filterName}.json'
 
-# wgEpsilon = 1e9
-# findWgIntersection_UpdateJSON(file, wgEpsilon, thetadegs)
+wgEpsilon = 1
+findWgIntersection_UpdateJSON(file, wgEpsilon, thetadegs)